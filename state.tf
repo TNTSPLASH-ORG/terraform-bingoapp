9de3516ed1b907a03539c61fca80c442e0c1c2a0
Darin Critchlow <darincritchlow@gmail.com>
2022-12-15 19:47:22 -0700

    Add terraform state in s3

--- state.tf
+++ state.tf
@@ -57,3 +57,13 @@ resource "aws_dynamodb_table" "bingoapp_terraform_state" {
   }
 }
 
+terraform {
+ backend "s3" {
+   bucket         = var.bucket_name
+   key            = "state/terraform.tfstate"
+   region         = "us-east-1"
+   encrypt        = true
+   kms_key_id     = "alias/terraform-bucket-key"
+   dynamodb_table = "terraform-state"
+ }
+}
